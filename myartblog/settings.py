bb693428c6af1b6ad016926b11dadd65d312e582
Robert Yin <31589648+brownyin365@users.noreply.github.com>
2019-09-29 03:00:55 -0700

    Update settings.py

--- myartblog/settings.py
+++ myartblog/settings.py
@@ -85,6 +85,7 @@ DATABASES = {
 }
 
 
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -120,6 +121,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
